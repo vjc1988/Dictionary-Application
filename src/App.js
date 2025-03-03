c56b9c7a30a05718e212301637148e80c6c0fd44
vjc1988 <victoriajaynecadd@yahoo.com>
2025-03-03 16:02:32 +0000

    Added links to footer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ export default function App() {
         <header className="App-header">
           <Dictionary />
         </header>
-        <footer>Coded with 🩷 by Victoria Cadd</footer>
+        <footer>
+          Coded with 🩷 by Victoria Cadd and is open-sourced on {""}
+          <a href="https://github.com/vjc1988/Dictionary-Application">
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://dictionary-app-vc.netlify.app/">Netlify</a>
+        </footer>
       </div>
     </div>
   );
